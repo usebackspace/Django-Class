938ee00e66148590c8fa2b139821fb1b74f18d3a
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2024-01-19 16:15:02 +0530

    update

--- Hero4_template/Hero4_template/settings.py
+++ Hero4_template/Hero4_template/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = Path(BASE_DIR,'templates')
+TEMPLATES_DIR = Path(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Hero4_template.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
